a3c0b12c26e75526bcae40716f2b3cde86db201e
VishrutMargam <vishrutm@axioned.com>
2021-12-01 12:07:19 +0530

    added fectch six data function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,27 @@ $(document).ready(function() {
     var n = 6;
     var content = '';
 
+    // calling fetch six data
+    fetchSixData(n)
+
+    // asynchronous fetch six data function
+    async function fetchSixData(n) {
+        for(i=n-5; i<=n; i++) {
+            if(i < 101) {
+                var url = `https://jsonplaceholder.typicode.com/posts/${i}`;
+                var data = await fetch(url).then(res => res.json())
+                displayList(data)
+            } else {
+                $('.load').hide();
+            }
+        }
+
+        $('html, body').animate({
+            scrollTop: $(document).height()
+        }, 'slow');
+    }
+
+
     // load more data functionality
     $('.load').click((e) => {
         e.preventDefault();
